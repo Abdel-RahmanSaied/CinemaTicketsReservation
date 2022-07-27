007d0c3a80194c4a724f74209a0193e7527fb3e5
Abdel-RahmanSaied <abdo.am169@gmail.com>
2022-07-27 02:15:22 +0200

    minix and generics complete

--- CinemaTicketsReservation/views.py
+++ CinemaTicketsReservation/views.py
@@ -4,6 +4,9 @@ from .models import *
 from rest_framework.decorators import api_view
 from rest_framework import status , filters
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework import  generics , mixins , viewsets
 from .serializers import *
 
 # Create your views here.
@@ -88,5 +91,141 @@ def FBV_PK(request,pk):
     elif request.method == 'DELETE':
             guest.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+
+#4.1 Class Based View
+class CBV_List(APIView) :
+    def get(self , request):
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests , many=True)
+        return Response(serializer.data)
+    def post(self,request):
+        serializer = GuestSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data , status=status.HTTP_400_BAD_REQUEST)
+
+#4.2 GET PUT DELETE --> PK
+
+class CBV_PK(APIView) :
+    def get_object(self , pk):
+        try :
+            return Guest.objects.get(pk=pk)
+        except Guest.DoesNotExist :
+            raise Http404
+
+    def get(self , request , pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest)
+        return  Response(serializer.data)
+    def put(self , request , pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest , data=request.data)
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self , request , pk):
+        guest = self.get_object(pk)
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# 5 Mixins
+# 5.1 mixins list
+
+class mixins_list(mixins.ListModelMixin , mixins.CreateModelMixin , generics.GenericAPIView ):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+    def get(self , request):
+        return self.list(request)
+
+    def post(self,request):
+        return self.create(request)
+
+#5.2 mixins Get put delete
+
+class mixins_pk(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+    def get(self , request , pk):
+        return self.retrieve(request)
+    def put(self,request , pk):
+        return self.update(request)
+    def delete(self,request , pk):
+        return self.destroy(request)
+
+
+# 6 Generics
+# 6.1  get and post
+
+class generics_list(generics.ListCreateAPIView) :
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+class generics_pk(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+
+
+# ViewSets
+
+class viewSets_guest(viewsets.ModelViewSet) :
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+class viewSets_movie(viewsets.ModelViewSet) :
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['movie']
+
+class viewSets_reservation(viewsets.ModelViewSet) :
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+#8 Find Movie
+@api_view(['GET'])
+def find_movie(request):
+    movie = request.data['movie']
+    hall = request.data['hall']
+    movies = Movie.objects.filter( movie = movie  , hall = hall)
+
+    serializer = MovieSerializer(movies , many= True)
+    return Response(serializer.data)
+
+#9 Create new Reservation
+
+@api_view(["POST"])
+def new_reservation(request):
+    movie = request.data['movie']
+    hall = request.data['hall']
+    name = request.data['name']
+    mobile = request.data['mobile']
+
+    guest = Guest()
+    reservation = Reservation()
+
+    try :
+        movies = Movie.objects.get(hall=hall , movie=movie)
+    except  Movie.DoesNotExist  :
+        return Response( {"Response":" invalid film data"} , status=status.HTTP_404_NOT_FOUND)
+
+    guest.name = name
+    guest.mobile = mobile
+    guest.save()
+
+    reservation.guest = guest
+    reservation.movie = movies
+    reservation.save()
+
+    return Response( {"Response":" Reservation Successfully "} , status=status.HTTP_201_CREATED)
+
 
 
--- cinemaSystem/urls.py
+++ cinemaSystem/urls.py
@@ -1,7 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from CinemaTicketsReservation import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('guests' , views.viewSets_guest)
+router.register('movies' , views.viewSets_movie)
+router.register('reservations' , views.viewSets_reservation)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,5 +24,33 @@ urlpatterns = [
 
     # 3.1 GET PK
     path('rest/FBV/<int:pk>' , views.FBV_PK),
+
+    # 4.1 GET POST from rest framework class based view
+    path('rest/CBVList/', views.CBV_List.as_view()),
+
+    # 4.2 GET PUT DELETE class based view --> PK
+    path('rest/CBVPK/<int:pk>', views.CBV_PK.as_view()),
+
+    # 5.1 GET POST mixins
+    path('rest/mixins_list/', views.mixins_list.as_view()),
+
+    # 5.2 GET put delete  mixins
+    path('rest/mixins_pk/<int:pk>', views.mixins_pk.as_view()),
+
+    # 6 Generics
+    # 6.1 GET POST
+    path('rest/generics_list/', views.generics_list.as_view()),
+
+    # 6.2 GET put delete
+    path('rest/generics_pk/<int:pk>', views.generics_pk.as_view()),
+
+    # ViewSets
+    path('rest/viewsets/', include(router.urls)),
+
+    # Find_Movies
+    path('fbv/findmovie/' , views.find_movie) ,
+    # new reservation
+    path('fbv/new_reservation/' , views.new_reservation)
+
 
 ]
